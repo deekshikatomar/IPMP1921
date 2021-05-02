6cdc0f449c15b89d2fc1f45ca1e5911d7a54e925
deekshikatomar <81900983+deekshikatomar@users.noreply.github.com>
2021-05-03 01:18:59 +0530

    Add files via upload

---
+++ week 5/binary search/Ubiquitous_Binary_ Search.cpp
@@ -0,0 +1,19 @@
+int BinarySearch(int A[], int l, int r, int key)
+{
+	int m;
+
+	while( l <= r )
+	{
+		m = l + (r-l)/2;
+
+		if( A[m] == key ) 
+			return m;
+
+		if( A[m] < key ) 
+			l = m + 1;
+		else
+			r = m - 1;
+	}
+
+	return -1;
+}
